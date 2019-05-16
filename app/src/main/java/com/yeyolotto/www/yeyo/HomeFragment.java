b2bf14f9d98ea0047c59b4bc3cb706abb38b95d6
Oisbel Simpson <oisbelsimpv@gmail.com>
2019-05-16 15:17:00 -0500

    Add last tiro to home fragment

--- app/src/main/java/com/yeyolotto/www/yeyo/HomeFragment.java
+++ app/src/main/java/com/yeyolotto/www/yeyo/HomeFragment.java
@@ -8,6 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.yeyolotto.www.yeyo.data.Tiro;
+import com.yeyolotto.www.yeyo.data.YeyoDbHelper;
+import com.yeyolotto.www.yeyo.utilities.DataUtils;
+
+import java.util.List;
 
 
 /**
@@ -15,7 +23,17 @@ import android.widget.ImageButton;
  */
 public class HomeFragment extends Fragment {
 
-    ImageButton userBT;
+    ImageButton userBT; // boton que muestra los datos de usuario
+    TextView lastDateUpdatedTV;
+    TextView loadingTV; // para mostrar errores o actualizsacion satisfactoria
+    ImageView occasionIV; // dia o noche del ultimo tiro
+    TextView numberTV; // ultimo tiro
+    TextView dateTV; // fecha del ultimo tiro
+
+    Tiro lastTiro;
+
+    /** Database helper that will provide us access to the database */
+    private YeyoDbHelper mDbHelper;
 
 
     public HomeFragment() {
@@ -30,6 +48,11 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         userBT = view.findViewById(R.id.userBT);
+        lastDateUpdatedTV = view.findViewById(R.id.lastDateUpdatedTV);
+        loadingTV = view.findViewById(R.id.loadingTV);
+        occasionIV = view.findViewById(R.id.occasionIV);
+        numberTV = view.findViewById(R.id.numberTV);
+        dateTV = view.findViewById(R.id.dateTV);
 
         userBT.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +62,44 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        mDbHelper = new YeyoDbHelper(getContext());
+
+        makeTiroQuery();
+
         return view;
+    }
+
+    /**
+     * Recupera el ultimo tiro de la base de datos
+     */
+    private void makeTiroQuery(){
+
+        List<Tiro> list = DataUtils.GetLastTiros(1,mDbHelper.getReadableDatabase());
+        if(list != null && list.size()>0) {
+            lastTiro = list.get(0);
+            if(lastTiro.getHora().equals("N"))
+                occasionIV.setImageResource(R.drawable.ic_action_night_today);
+            else
+                occasionIV.setImageResource(R.drawable.ic_action_day_today);
+            lastDateUpdatedTV.setText(lastTiro.getFecha());
+            dateTV.setText(lastTiro.getFecha());
+            numberTV.setText(lastTiro.getTiro());
+        }else {
+            showErrorMessage();
+        }
+    }
+
+    private void showErrorMessage(){
+        loadingTV.setText(getString(R.string.loadingError));
+        loadingTV.setBackgroundResource(R.color.loadingError);
+        loadingTV.setVisibility(View.VISIBLE);
+    }
+
+    private void showLoadingMessage(){
+        loadingTV.setVisibility(View.VISIBLE);
+        loadingTV.setText(getString(R.string.loading));
+        loadingTV.setBackgroundResource(R.color.loading);
     }
 
 }
